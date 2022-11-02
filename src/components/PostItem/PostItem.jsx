98507c4367fd8ae7fb4a81402a0974605d7bf9fa
Elena Ustinova <elena.u@fireart-d.com>
2022-11-02 16:00:24 +0300

    Add post style

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "../../styles/PostItem.css";
 
 const PostItem = (props) => {
     return (
-        <div>
-            <h1>{props.recipe.title}</h1>
+        <div className="post">
+            <h1 className="post__title" >{props.recipe.title}</h1>
             <p>
                 {props.recipe.body}
             </p>
-            <button onClick={() => props.remove(props.recipe)} >Delete</button>
+            <button onClick={() => props.remove(props.recipe)} className="post__delete-button">Delete</button>
         </div>
     )
 };
